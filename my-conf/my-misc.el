8d35efdbe962995e034ef4c0c59c93ff3b3e41e2
Jiri Marsik <jiri.marsik89@gmail.com>
2013-07-16 01:10:00 +0200

    Change fill-column to 78 to fit default Emacs frame width

--- my-conf/my-misc.el
+++ my-conf/my-misc.el
@@ -4,8 +4,8 @@
 ;; Flash the frame on bell.
 (setq visible-bell t)
 
-;; Set fill column limit to 80 (instead of the default 70).
-(setq-default fill-column 80)
+;; Set fill column limit to 78 (instead of the default 70).
+(setq-default fill-column 78)
 
 ;; Always show column number as well as line number.
 (column-number-mode)
