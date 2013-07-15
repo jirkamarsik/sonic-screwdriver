3aba4a3ef5ec0953f75d51a9c3ffcc1c55034146
Jiri Marsik <jiri.marsik89@gmail.com>
2013-07-16 01:20:36 +0200

    Fixed dropped quote

--- init.el
+++ init.el
@@ -2,20 +2,20 @@
 (add-to-list 'load-path "~/.emacs.d/my-conf")
 
 ;; Load all the parts of my configuration.
-(dolist (section-name ("defuns"
-                       "pkgs"
-                       "ido"
-                       "smex"
-                       "evil"
-                       "prog"
-                       "elisp"
-                       "clojure"
-                       "spell"
-                       "text"
-                       "latex"
-                       "markdown"
-                       "git"
-                       "misc"
-                       "bindings"
-                       "theme"))
+(dolist (section-name '("defuns"
+                        "pkgs"
+                        "ido"
+                        "smex"
+                        "evil"
+                        "prog"
+                        "elisp"
+                        "clojure"
+                        "spell"
+                        "text"
+                        "latex"
+                        "markdown"
+                        "git"
+                        "misc"
+                        "bindings"
+                        "theme"))
   (load (concat "my-" section-name)))
