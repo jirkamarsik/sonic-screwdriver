4846e589862d758dd793e4382a6d9da686160285
Jiri Marsik <jiri.marsik89@gmail.com>
2013-07-16 02:36:45 +0200

    Fixed triggering of FlySpell

--- my-conf/my-latex.el
+++ my-conf/my-latex.el
@@ -1,7 +1,5 @@
 ;; Turn on automatic line breaks in latex-mode.
-(add-hook 'latex-mode-hook 'turn-on-auto-fill)
+(add-hook 'latex-mode-hook 'auto-fill-mode)
 
 ;; Turn on on-the-fly spell-checking in latex-mode.
-(eval-after-load "ispell"
-  '(when (executable-find ispell-program-name)
-     (add-hook 'latex-mode-hook 'turn-on-flyspell)))
+(add-hook 'latex-mode-hook 'flyspell-mode)
--- my-conf/my-text.el
+++ my-conf/my-text.el
@@ -1,7 +1,5 @@
 ;; Turn on automatic line breaks in text-mode.
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
+(add-hook 'text-mode-hook 'auto-fill-mode)
 
 ;; Turn on on-the-fly spell-checking in text-mode.
-(eval-after-load "ispell"
-  '(when (executable-find ispell-program-name)
-     (add-hook 'text-mode-hook 'turn-on-flyspell)))
+(add-hook 'text-mode-hook 'flyspell-mode)
