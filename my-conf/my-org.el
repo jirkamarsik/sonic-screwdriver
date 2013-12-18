7284280d34a3ffc3fd8a053193c620593521cd4c
Jiri Marsik <jiri.marsik89@gmail.com>
2013-12-18 10:29:56 +0100

    Loading org-mode...

---
+++ my-conf/my-org.el
@@ -0,0 +1,6 @@
+;; Copied over from the Org-mode tutorial.
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
