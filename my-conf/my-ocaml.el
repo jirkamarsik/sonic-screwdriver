08006f24351fd1310d1906936ed9bb63a3ff897b
Jiri Marsik <jiri.marsik89@gmail.com>
2013-12-25 17:51:27 +0100

    Delay setting up OCaml after tuareg has been loaded

--- my-conf/my-ocaml.el
+++ my-conf/my-ocaml.el
@@ -1,30 +1,37 @@
 ;;; Setup from https://github.com/diml/utop#integration-with-emacs
 
-;; Setup environment variables using opam
-(dolist (var (car (read-from-string (shell-command-to-string "opam config env --sexp"))))
-  (setenv (car var) (cadr var)))
+(defun initialize-my-ocaml ()
+  "Setup tuareg-mode to use utop, ocp-indent and merlin from opam."
 
-;; Update the emacs path
-(setq exec-path (split-string (getenv "PATH") path-separator))
+  ;; Setup environment variables using opam
+  (dolist (var (car (read-from-string (shell-command-to-string
+                                       "opam config env --sexp"))))
+    (setenv (car var) (cadr var)))
 
-;; Update the emacs load path
-(push (concat (getenv "OCAML_TOPLEVEL_PATH") "/../../share/emacs/site-lisp") load-path)
+  ;; Update the emacs path
+  (setq exec-path (split-string (getenv "PATH") path-separator))
 
-;; Automatically load utop.el
-(autoload 'utop "utop" "Toplevel for OCaml" t)
-(autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t)
-(add-hook 'tuareg-mode-hook 'utop-setup-ocaml-buffer)
+  ;; Update the emacs load path
+  (push (concat (getenv "OCAML_TOPLEVEL_PATH") "/../../share/emacs/site-lisp")
+        load-path)
 
-;; Tuareg seems to force tuareg-run-ocaml over utop, which doesn't work.
-(define-key tuareg-mode-map (kbd "C-c C-s") 'utop)
+  ;; Automatically load utop.el
+  (autoload 'utop "utop" "Toplevel for OCaml" t)
+  (autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t)
+  (add-hook 'tuareg-mode-hook 'utop-setup-ocaml-buffer)
+
+  ;; Tuareg seems to force tuareg-run-ocaml over utop, which doesn't work.
+  (define-key tuareg-mode-map (kbd "C-c C-s") 'utop)
 
 
-;;; Setting up ocp-indent and merlin from OPAM
+  ;; Setting up ocp-indent and merlin from OPAM
 
-(setq opam-share (substring (shell-command-to-string "opam config var share") 0 -1))
-(add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+  (setq opam-share (substring (shell-command-to-string "opam config var share") 0 -1))
+  (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
 
-(require 'ocp-indent)
+  (require 'ocp-indent)
 
-(require 'merlin)
-(add-hook 'tuareg-mode-hook 'merlin-mode)
+  (require 'merlin)
+  (add-hook 'tuareg-mode-hook 'merlin-mode))
+
+(eval-after-load 'tuareg (function initialize-my-ocaml))
