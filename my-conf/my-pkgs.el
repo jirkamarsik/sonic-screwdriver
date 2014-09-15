60990f052a52745803e391a7faf005bbb968a15a
Jiri Marsik <jiri.marsik89@gmail.com>
2014-09-15 14:14:10 +0200

    BUGFIX: Use package-filter before fetching archive contents

--- my-conf/my-pkgs.el
+++ my-conf/my-pkgs.el
@@ -1,25 +1,28 @@
-;; Setup package.el to use MELPA and MELPA-STABLE (http://melpa.milkbox.net/).
+;; Setup package.el to use Melpa, Melpa-stable and Marmalade.
 (require 'package)
+
 (add-to-list 'package-archives
   '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
   '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
-;; Also add Marmalade (e.g. for nrepl-discover).
 (add-to-list 'package-archives
   '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+
+;; Install and setup package-filter before fetching arhive contents...
+(require 'package-filter)
+
+;; Use ghc from MELPA-STABLE so that it is in line with cabal ghc-mod.
+(setq package-archive-exclude-alist '(("melpa" . (ghc))))
+
+
+;; Load the archive contents.
 (package-initialize)
 
-;; Download the package list from MELPA.
+;; Download the package lists. (redundant?)
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; Install package-filter before proceeding
-(when (not (package-installed-p 'package-filter))
- (progn
-  (switch-to-buffer
-   (url-retrieve-synchronously
-    "https://raw.github.com/milkypostman/package-filter/master/package-filter.el"))
-   (package-install-from-buffer (package-buffer-info) 'single)))
 
 ;; Install the desired packages.
 (defvar my-packages '(better-defaults
@@ -49,8 +52,6 @@
                       fsharp-mode)
   "A list of packages that should be installed.")
 
-;; Use ghc from MELPA-STABLE so that it is in line with cabal ghc-mod.
-(setq package-archive-exclude-alist '(("melpa" . (ghc))))
 
 ;; Check and install all the packages.
 (dolist (p my-packages)
