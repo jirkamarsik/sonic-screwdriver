57f0d3fce659a3bf6316083dc0a8ae5448027364
Jirka Marsik <jiri.marsik89@gmail.com>
2014-12-29 11:09:45 +0100

    Use ghci-ng

--- init.el
+++ init.el
@@ -43,6 +43,7 @@
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(haskell-process-args-cabal-repl (quote ("--ghc-option=-ferror-spans" "--with-ghc=ghci-ng")))
  '(haskell-process-type (quote cabal-repl)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
