a816e8168e532db4351cd4b5afcd941bdd6f39a0
Jirka Marsik <jiri.marsik89@gmail.com>
2015-04-23 16:04:15 +0200

    Do not reference haskell-mode-map before loading haskell-mode

--- my-conf/my-haskell.el
+++ my-conf/my-haskell.el
@@ -18,14 +18,16 @@
 
 
 ;; Use haskell-interactive-mode instead of inferior-haskell-mode
-(define-key haskell-mode-map (kbd "C-x C-d") nil)
-(define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
-(define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
-(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-(define-key haskell-mode-map (kbd "C-c M-.") nil)
-(define-key haskell-mode-map (kbd "C-c C-d") nil)
+(eval-after-load "haskell"
+  '(progn
+     (define-key haskell-mode-map (kbd "C-x C-d") nil)
+     (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+     (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+     (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
+     (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+     (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+     (define-key haskell-mode-map (kbd "C-c M-.") nil)
+     (define-key haskell-mode-map (kbd "C-c C-d") nil)))
 
 
 ;; ghc-mod
@@ -36,4 +38,5 @@
 
 
 ;; hindent
-(define-key haskell-mode-map (kbd "C-c i") 'hindent/reformat-decl)
+(eval-after-load "haskell"
+  '(define-key haskell-mode-map (kbd "C-c i") 'hindent/reformat-decl))
