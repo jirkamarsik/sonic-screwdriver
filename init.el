0fb31258bd3671c4763e508e62bc5217fa4a6419
Jirka Marsik <jiri.marsik89@gmail.com>
2015-05-06 11:18:01 +0200

    Look for Agda Stdlib in .nix-profile

--- init.el
+++ init.el
@@ -45,8 +45,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(agda2-include-dirs (quote ("." "/home/jirka/.nix-profile/share/agda")))
  '(browse-url-browser-function (quote browse-url-generic))
- '(agda2-include-dirs (quote ("." "/home/jirka/Projects/cufp/agda-prelude/src")))
  '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes
    (quote
