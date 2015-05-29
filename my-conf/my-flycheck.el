7286bdc81ea3797d8c2056e867e0a20cc7334bfe
Jirka Marsik <jiri.marsik89@gmail.com>
2015-05-30 01:07:16 +0200

    Disable liquid-haskell Flycheck

--- my-conf/my-flycheck.el
+++ my-conf/my-flycheck.el
@@ -2,11 +2,4 @@
 
 (eval-after-load 'flycheck
   '(progn
-     (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup)
-
-     ;; Not using proceed-with-executable, since I always want to use my
-     ;; patched version of flycheck-liquid.el.
-     (when (executable-find "liquid")
-       (require 'flycheck-liquid)
-       (flycheck-add-next-checker 'haskell-ghc '(warning . haskell-liquid) 'append)
-       (add-to-list 'flycheck-disabled-checkers 'haskell-liquid))))
+     (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup)))
