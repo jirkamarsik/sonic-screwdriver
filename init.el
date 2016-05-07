743ed0b7accbb98ed9f6deb9c39493d6dd0beb19
Jirka Marsik <jiri.marsik89@gmail.com>
2016-05-08 00:03:04 +0200

    Switch to solarized-light

--- init.el
+++ init.el
@@ -48,7 +48,7 @@
  ;; If there is more than one, they won't work right.
  '(agda2-include-dirs (quote ("." "/home/jirka/Projects/cufp/agda-prelude/src")))
  '(browse-url-browser-function (quote browse-url-generic))
- '(custom-enabled-themes (quote (solarized-dark)))
+ '(custom-enabled-themes (quote (solarized-light)))
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
