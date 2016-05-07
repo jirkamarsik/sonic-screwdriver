1797a56a3c8a10e7322b85a6767bdaf0bc6e2d9f
Jirka Marsik <jiri.marsik89@gmail.com>
2016-05-08 00:01:34 +0200

    Customize: drop cabal-repl

--- init.el
+++ init.el
@@ -52,7 +52,6 @@
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(haskell-process-type (quote cabal-repl))
  '(hindent-style "chris-done")
  '(idris-interpreter-path
    "/home/jirka/cabal-sandboxes/Idris/.cabal-sandbox/bin/idris")
